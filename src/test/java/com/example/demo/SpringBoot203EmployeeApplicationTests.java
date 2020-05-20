fa4d63b27bc3baf06cae94618c3095183c44c41c
fitsumtsehay <tsehayfitsum@gmail.com>
2020-05-19 20:17:17 -0400

    EmployeeForm

---
+++ src/test/java/com/example/demo/SpringBoot203EmployeeApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class SpringBoot203EmployeeApplicationTests {
+public static void main(String[] args) {
+    SpringApplication.run(SpringBoot203EmployeeApplication.class, args);
+}
+    }
+
+
